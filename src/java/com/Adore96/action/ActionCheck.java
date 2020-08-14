498535b0ad1dfc1a5ac11b2677ae4bdf70b6a066
Kalhara Perera <adorekasun@gmail.com>
2020-08-14 18:21:01 +0530

    Struts.xml and packages updated

--- src/java/com/Adore96/action/ActionCheck.java
+++ src/java/com/Adore96/action/ActionCheck.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package action;
+package com.Adore96.action;
 
+import static com.opensymphony.xwork2.Action.ERROR;
 import static com.opensymphony.xwork2.Action.SUCCESS;
+import org.jboss.weld.event.Status;
 
 /**
  *
@@ -14,6 +16,6 @@ import static com.opensymphony.xwork2.Action.SUCCESS;
 public class ActionCheck {
     public String execute(){
         System.out.println("Execute from actioncheck");
-        return SUCCESS;
+        return "success";
     }
 }
