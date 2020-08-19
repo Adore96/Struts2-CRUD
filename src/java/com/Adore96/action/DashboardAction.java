4f9e7086f62263c9428fa523cfce9e6d0510a67b
Kalhara Perera <adorekasun@gmail.com>
2020-08-19 10:44:34 +0530

    ShowTableData Done.

--- src/java/com/Adore96/action/DashboardAction.java
+++ src/java/com/Adore96/action/DashboardAction.java
@@ -7,7 +7,9 @@ package com.Adore96.action;
 
 import com.Adore96.dao.StudentDAO;
 import com.Adore96.model.StudentInfo;
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
 import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.apache.struts2.ServletActionContext;
@@ -17,12 +19,23 @@ import org.apache.struts2.ServletActionContext;
  * @author kasun_k
  */
 public class DashboardAction {
-     StudentDAO studentDAO = new StudentDAO();
-     HttpServletRequest request = ServletActionContext.getRequest();
+
+    public String ListUser() {
+        System.out.println("Listuser method called in Dashboard action.");
+        StudentDAO studentDAO = new StudentDAO();
+        HttpServletRequest request = ServletActionContext.getRequest();
 
         List<StudentInfo> listUser = studentDAO.ShowTable();
         HttpSession session = request.getSession(false);
 
 //        list user error
-        request.setAttribute("listUser", listUser);
+        if (listUser.isEmpty()) {
+            System.out.println("List user is empty.");
+            return "emptylist";
+        } else {
+            request.setAttribute("listUser", listUser);
+            return "showtabledata";
+        }
+
+    }
 }
